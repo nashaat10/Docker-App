5a29dd809e5557106142665da60f4fa510b2279a
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-09-30 23:34:53 +0300

    create docker file and build it

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 const PORT = 3000;
+
+app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
+
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
