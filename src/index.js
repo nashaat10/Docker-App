75944b3d52edd25d029a31e9b025f97f981a4a5b
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-10-01 15:40:22 +0300

    add

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
+app.get("/", (req, res) => res.send("<h1>Hello World mohamed!</h1>"));
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
